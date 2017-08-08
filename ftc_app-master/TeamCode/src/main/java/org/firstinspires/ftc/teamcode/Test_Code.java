89bbb33e3a725fa4b5d183a7a1ba47ec701095fb
Mr. Quick <andrew.quick@gmail.com>
2017-08-07 21:25:21 -0400

    8/7/2017
    Add Drive motor and Arm motor

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Code.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Code.java
@@ -9,11 +9,19 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class Test_Code extends OpMode {
 
 DcMotor Testmotor;
+    DcMotor Testmotor2;
+    DcMotor Arm;
     double testpower;
+    double testpower2;
+    double armpower = .3;
+
 
     @Override
     public void init() {
         Testmotor = (DcMotor) hardwareMap.dcMotor.get("test");
+        Testmotor2 = (DcMotor) hardwareMap.dcMotor.get("test2");
+        Arm = (DcMotor) hardwareMap.dcMotor.get("arm");
+
 
     }
 
@@ -21,8 +29,23 @@ DcMotor Testmotor;
     public void loop() {
 
         testpower = gamepad1.left_stick_y;
+        testpower2 = gamepad1.right_stick_y;
+
 
         Testmotor.setPower(testpower);
+        Testmotor2.setPower(testpower2);
+
+        if (gamepad1.a)
+            Arm.setPower(armpower);
+        else
+            Arm.setPower(0.0);
+
+        if (gamepad1.b)
+            Arm.setPower(1.0);
+        else
+            Arm.setPower(0.0);
+
+
 
 
     }
